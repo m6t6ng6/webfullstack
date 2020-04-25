f52586f4c3fe61712b62c4ed23209c256f96d19e
m6t6ng6 <48723981+m6t6ng6@users.noreply.github.com>
2020-04-25 17:43:05 -0300

    modificaciones

---
+++ uade-24-abril-2020/js/ejercicio1.js
@@ -0,0 +1,11 @@
+// item 1
+$(document).ready(function(){
+    $("body").addClass('destacado');
+});
+
+// item 2
+$("h1").removeClass('destacado2');
+
+// item 3
+var valor = $("input").val();
+console.log(valor);
