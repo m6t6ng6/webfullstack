22ccb2db7fcae7cf026d9c2fa648bd440734555e
Fernando Bustamante <ferna2909@hotmail.com>
2020-04-27 00:27:06 -0300

    higiene del ejercicio2

--- uade-24-abril-2020/js/ejercicio2.js
+++ uade-24-abril-2020/js/ejercicio2.js
@@ -1,14 +1,6 @@
-// item 1
 $(document).ready(function(){
     //$("body").addClass('destacado');
 });
-
-// item 2
-// $("h1").removeClass('destacado2');
-
-// item 3
-// var valor = $("input").val();
-// console.log(valor);
 
 $(".formError").hide();
 $("#nombre_ayuda").hide();
